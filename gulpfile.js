ecd048bcddfcb498ce32759f1eb466db9b7edfc3
Ganiev Davlet <Ganiev.Davlet@gmail.com>
2018-05-15 10:25:11 +0300

    setting gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,30 @@
-var gulp = require('gulp');
-// var uglify = require('gulp-uglify');
-// var concat = require('gulp-concat');
-// var rename = require('gulp-rename');
+const gulp = require('gulp');
+const pug = require('gulp-pug');
 
-gulp.task('sass', function(){
-  return gulp.src('dev/*.js')
-  .pipe(concat('app.js'))
-  .pipe(uglify())
-  .pipe(rename({
-  suffix: '.min'
-  }))
-  .pipe(gulp.dest('build/js'));
- });
+const paths = {
+  root: './build',
+  templates: {
+    pages: 'src/templates/pages/*.pug',
+    src: 'src/templates/**/*.pug',
+    dest: 'build/assets/'
+  }
+}
+
+//pug
+function templates() {
+  return gulp.src(paths.templates.pages)
+    .pipe(pug({pretty: true}))
+    .pipe(gulp.dest(paths.root));
+}
+
+exports.templates = templates;
+
+// gulp.task('sass', function(){
+//   return gulp.src('dev/*.js')
+//   .pipe(concat('app.js'))
+//   .pipe(uglify())
+//   .pipe(rename({
+//   suffix: '.min'
+//   }))
+//   .pipe(gulp.dest('build/js'));
+// });
